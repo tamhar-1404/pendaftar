602588e1f8caf68ea1b9cf08b5688e71b6dad12d
Kader2637 <kaderofficial33@gmail.com>
2023-06-13 13:14:02 +0700

    admin_dashboard

---
+++ app/Http/Controllers/DashboardSiswaController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Dashboard_siswa;
+use App\Http\Requests\StoreDashboard_siswaRequest;
+use App\Http\Requests\UpdateDashboard_siswaRequest;
+
+class DashboardSiswaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('dashboard_siswa.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreDashboard_siswaRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreDashboard_siswaRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Dashboard_siswa  $dashboard_siswa
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Dashboard_siswa $dashboard_siswa)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Dashboard_siswa  $dashboard_siswa
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Dashboard_siswa $dashboard_siswa)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateDashboard_siswaRequest  $request
+     * @param  \App\Models\Dashboard_siswa  $dashboard_siswa
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateDashboard_siswaRequest $request, Dashboard_siswa $dashboard_siswa)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Dashboard_siswa  $dashboard_siswa
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Dashboard_siswa $dashboard_siswa)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/dashboard_admin', App\Http\Controllers\DashboardController::class);
+Route::resource('/dashboard_siswa', App\Http\Controllers\DashboardSiswaController::class);
+
