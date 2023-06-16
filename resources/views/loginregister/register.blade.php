ef71c5591b5f22b65f3663e9a262e4f302207311
dimassaja87 <122768607+dimassaja87@users.noreply.github.com>
2023-06-16 11:04:51 +0700

    register

--- resources/views/loginregister/register.blade.php
+++ resources/views/loginregister/register.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <!-- Mirrored from lineone.piniastudio.com/pages-singup-2.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 10 May 2023 04:16:45 GMT -->
 <!-- Added by HTTrack -->
 <meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
@@ -14,6 +13,7 @@
 
     <title>Daftar</title>
     <link rel="icon" type="image/png" href="lineone/images/favicon.png" />
+    @vite('resources/css/app.css')
 
     <!-- CSS Assets -->
     <link rel="stylesheet" href="lineone/css/app.css" />
@@ -31,8 +31,7 @@
         /**
          * THIS SCRIPT REQUIRED FOR PREVENT FLICKERING IN SOME BROWSERS
          */
-        localStorage.getItem("_x_darkMode_on") === "true" &&
-            document.documentElement.classList.add("dark");
+        localStorage.getItem("_x_darkMode_on") === "true" && document.documentElement.classList.add("dark");
     </script>
 </head>
 
@@ -46,7 +45,7 @@
     <div id="root" class="min-h-100vh flex grow bg-slate-50 dark:bg-navy-900" x-cloak>
         <div class="fixed top-0 hidden p-6 lg:block lg:px-12">
             <a href="#" class="flex items-center space-x-2">
-                <img class="h-12 w-12" src="lineone/images/hummasoft2.svg" alt="logo" />
+                <img class="h-14 w-100" src="lineone/images/hummasoft2.png" alt="logo" />
             </a>
         </div>
         <div class="hidden w-full place-items-center lg:grid">
@@ -56,6 +55,50 @@
                 <img class="w-full" x-show="$store.global.isDarkModeEnabled"
                     src="lineone/images/illustrations/dashboard-meet-dark.svg" alt="image" />
             </div>
+
+        </div>
+        <div class="col-span-12 grid lg:col-span-6 lg:place-items-center">
+            <ol class="steps is-vertical line-space [--size:2.75rem] [--line:.5rem]">
+                <li class="step space-x-4 pb-12 before:bg-slate-200 dark:before:bg-navy-500">
+                    <div class="step-header mask is-hexagon bg-info text-white dark:bg-accent">
+                        <i class="fa-solid fa-layer-group text-base"></i>
+                    </div>
+                    <div class="text-left">
+                        <p class="text-xs text-slate-400 dark:text-navy-300">Step 1</p>
+                        <h3 class="text-base font-medium text-info dark:text-accent-light">Data Diri</h3>
+                    </div>
+                </li>
+                <li class="step space-x-4 pb-12 before:bg-slate-200 dark:before:bg-navy-500">
+                    <div
+                        class="step-header mask is-hexagon bg-slate-200 text-slate-500 dark:bg-navy-500 dark:text-navy-100">
+                        <i class="fa-solid fa-user text-base"></i>
+                    </div>
+                    <div class="text-left">
+                        <p class="text-xs text-slate-400 dark:text-navy-300">Step 2</p>
+                        <h3 class="text-base font-medium">Data Diri</h3>
+                    </div>
+                </li>
+                <li class="step space-x-4 pb-12 before:bg-slate-200 dark:before:bg-navy-500">
+                    <div
+                        class="step-header mask is-hexagon bg-slate-200 text-slate-500 dark:bg-navy-500 dark:text-navy-100">
+                        <i class="fa-solid fa-file-lines text-base"></i>
+                    </div>
+                    <div class="text-left">
+                        <p class="text-xs text-slate-400 dark:text-navy-300">Step 3</p>
+                        <h3 class="text-base font-medium">Berkas</h3>
+                    </div>
+                </li>
+                <li class="step space-x-4 before:bg-slate-200 dark:before:bg-navy-500">
+                    <div
+                        class="step-header mask is-hexagon bg-slate-200 text-slate-500 dark:bg-navy-500 dark:text-navy-100">
+                        <i class="fa-solid fa-check text-base"></i>
+                    </div>
+                    <div class="text-left">
+                        <p class="text-xs text-slate-400 dark:text-navy-300">Step 4</p>
+                        <h3 class="text-base font-medium">Konfirmasi</h3>
+                    </div>
+                </li>
+            </ol>
         </div>
         <main class="flex w-full flex-col items-center bg-white dark:bg-navy-700 lg:max-w-md">
             <div class="flex w-full max-w-sm grow flex-col justify-center p-5">
@@ -63,110 +106,102 @@
                     <img class="mx-auto h-16 w-16 lg:hidden" src="lineone/images/app-logo.svg" alt="logo" />
                     <div class="mt-4">
                         <h2 class="text-2xl font-semibold text-slate-600 dark:text-navy-100">
-                            Welcome To Lineone
+                            Selamat Datang
                         </h2>
                         <p class="text-slate-400 dark:text-navy-300">
-                            Please sign up to continue
+                            di Hummasoft Technology
                         </p>
                     </div>
-                </div>
-
-                <div class="mt-10 flex space-x-4">
-                    <button
-                        class="btn w-full space-x-3 border border-slate-300 font-medium text-slate-800 hover:bg-slate-150 focus:bg-slate-150 active:bg-slate-150/80 dark:border-navy-450 dark:text-navy-50 dark:hover:bg-navy-500 dark:focus:bg-navy-500 dark:active:bg-navy-500/90">
-                        <img class="h-5.5 w-5.5" src="images/logos/google.svg" alt="logo" />
-                        <span>Google</span>
-                    </button>
-                    <button
-                        class="btn w-full space-x-3 border border-slate-300 font-medium text-slate-800 hover:bg-slate-150 focus:bg-slate-150 active:bg-slate-150/80 dark:border-navy-450 dark:text-navy-50 dark:hover:bg-navy-500 dark:focus:bg-navy-500 dark:active:bg-navy-500/90">
-                        <img class="h-5.5 w-5.5" src="images/logos/github.svg" alt="logo" />
-                        <span>Github</span>
-                    </button>
-                </div>
-                <div class="my-7 flex items-center space-x-3">
-                    <div class="h-px flex-1 bg-slate-200 dark:bg-navy-500"></div>
-                    <p class="text-tiny+ uppercase">or sign up with email</p>
-
-                    <div class="h-px flex-1 bg-slate-200 dark:bg-navy-500"></div>
                 </div>
                 <div class="mt-4 space-y-4">
-                    <label class="relative flex">
-                        <input
-                            class="form-input peer w-full rounded-lg bg-slate-150 px-3 py-2 pl-9 ring-primary/50 placeholder:text-slate-400 hover:bg-slate-200 focus:ring dark:bg-navy-900/90 dark:ring-accent/50 dark:placeholder:text-navy-300 dark:hover:bg-navy-900 dark:focus:bg-navy-900"
-                            placeholder="Username" type="text" />
-                        <span
-                            class="pointer-events-none absolute flex h-full w-10 items-center justify-center text-slate-400 peer-focus:text-primary dark:text-navy-300 dark:peer-focus:text-accent">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 transition-colors duration-200"
-                                fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-                            </svg>
+                    <label class="block">
+                        <span>Nama</span>
+                        <span class="relative mt-1.5 flex">
+                            <input
+                                class="form-input peer w-full rounded-lg border border-slate-300 bg-transparent px-3 py-2 placeholder:text-slate-400/70 hover:border-slate-400 focus:border-primary dark:border-navy-450 dark:hover:border-navy-400 dark:focus:border-accent"
+                                placeholder="Masukkan nama anda" type="text" />
                         </span>
                     </label>
-                    <label class="relative flex">
-                        <input
-                            class="form-input peer w-full rounded-lg bg-slate-150 px-3 py-2 pl-9 ring-primary/50 placeholder:text-slate-400 hover:bg-slate-200 focus:ring dark:bg-navy-900/90 dark:ring-accent/50 dark:placeholder:text-navy-300 dark:hover:bg-navy-900 dark:focus:bg-navy-900"
-                            placeholder="Email" type="email" />
-                        <span
-                            class="pointer-events-none absolute flex h-full w-10 items-center justify-center text-slate-400 peer-focus:text-primary dark:text-navy-300 dark:peer-focus:text-accent">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 transition-colors duration-200"
-                                fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
-                                    d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-                            </svg>
+                </div>
+                <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 mt-4">
+                    <label class="block">
+                        <span>Tempat</span>
+                        <span class="relative mt-1.5 flex">
+                            <input
+                                class="form-input peer w-full rounded-lg border border-slate-300 bg-transparent px-3 py-2 placeholder:text-slate-400/70 hover:border-slate-400 focus:border-primary dark:border-navy-450 dark:hover:border-navy-400 dark:focus:border-accent"
+                                placeholder="Kota anda" type="text" />
                         </span>
                     </label>
-                    <label class="relative flex">
-                        <input
-                            class="form-input peer w-full rounded-lg bg-slate-150 px-3 py-2 pl-9 ring-primary/50 placeholder:text-slate-400 hover:bg-slate-200 focus:ring dark:bg-navy-900/90 dark:ring-accent/50 dark:placeholder:text-navy-300 dark:hover:bg-navy-900 dark:focus:bg-navy-900"
-                            placeholder="Password" type="password" />
-                        <span
-                            class="pointer-events-none absolute flex h-full w-10 items-center justify-center text-slate-400 peer-focus:text-primary dark:text-navy-300 dark:peer-focus:text-accent">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 transition-colors duration-200"
-                                fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
-                                    d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
-                            </svg>
+                    <label class="block">
+                        <span>Tanggal lahir</span>
+                        <span class="relative mt-1.5 flex">
+                            <input x-init="$el._x_flatpickr = flatpickr($el)"
+                                class="form-input peer w-full rounded-lg border border-slate-300 bg-transparent px-3 py-2 pl-9 placeholder:text-slate-400/70 hover:border-slate-400 focus:border-primary dark:border-navy-450 dark:hover:border-navy-400 dark:focus:border-accent"
+                                placeholder="Choose date..." type="text" />
+                            <span
+                                class="pointer-events-none absolute flex h-full w-10 items-center justify-center text-slate-400 peer-focus:text-primary dark:text-navy-300 dark:peer-focus:text-accent">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 transition-colors duration-200"
+                                    fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                                </svg>
+                            </span>
                         </span>
                     </label>
-                    <label class="relative flex">
-                        <input
-                            class="form-input peer w-full rounded-lg bg-slate-150 px-3 py-2 pl-9 ring-primary/50 placeholder:text-slate-400 hover:bg-slate-200 focus:ring dark:bg-navy-900/90 dark:ring-accent/50 dark:placeholder:text-navy-300 dark:hover:bg-navy-900 dark:focus:bg-navy-900"
-                            placeholder="Repeat Password" type="password" />
-                        <span
-                            class="pointer-events-none absolute flex h-full w-10 items-center justify-center text-slate-400 peer-focus:text-primary dark:text-navy-300 dark:peer-focus:text-accent">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 transition-colors duration-200"
-                                fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
-                                    d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
-                            </svg>
+                </div>
+                <div class="mt-4 space-y-4">
+                    <label class="block">
+                        <span>Kelas</span>
+                        <span class="relative mt-1.5 flex">
+                            <input
+                                class="form-input peer w-full rounded-lg border border-slate-300 bg-transparent px-3 py-2 placeholder:text-slate-400/70 hover:border-slate-400 focus:border-primary dark:border-navy-450 dark:hover:border-navy-400 dark:focus:border-accent"
+                                placeholder="Masukkan Kelas anda" type="text" />
                         </span>
                     </label>
-                    <div class="mt-4 flex items-center space-x-2">
-                        <input
-                            class="form-checkbox is-basic h-5 w-5 rounded border-slate-400/70 checked:border-primary checked:bg-primary hover:border-primary focus:border-primary dark:border-navy-400 dark:checked:border-accent dark:checked:bg-accent dark:hover:border-accent dark:focus:border-accent"
-                            type="checkbox" />
-                        <p class="line-clamp-1">
-                            I agree with
-                            <a href="#" class="text-slate-400 hover:underline dark:text-navy-300">
-                                privacy policy
-                            </a>
-                        </p>
-                    </div>
+                </div>
+                <div class="mt-4 space-y-4">
+                    <label class="block">
+                        <span>NISN</span>
+                        <span class="relative mt-1.5 flex">
+                            <input
+                                class="form-input peer w-full rounded-lg border border-slate-300 bg-transparent px-3 py-2 placeholder:text-slate-400/70 hover:border-slate-400 focus:border-primary dark:border-navy-450 dark:hover:border-navy-400 dark:focus:border-accent"
+                                placeholder="Masukkan NISN anda" type="text" />
+                        </span>
+                    </label>
+                </div>
+                <div class="mt-4 space-y-4">
+                    <label class="block">
+                        <span>Jenis Kelamin</span>
+                        <div class="mt-2 grid grid-cols-2 place-items-start gap-6 sm:grid-cols-6">
+                            <label class="inline-flex items-center space-x-2">
+                                <input
+                                    class="form-radio is-outline h-5 w-5 rounded-full border-slate-400/70 before:!bg-info checked:!border-info hover:!border-info focus:!border-info dark:border-navy-400"
+                                    name="outline_squircle" type="radio" />
+                                <span>Laki - laki</span>
+                            </label>
+                            <label class="inline-flex items-center space-x-2">
+                                <input
+                                    class="form-radio is-outline h-5 w-5 rounded-full border-slate-400/70 before:!bg-info checked:!border-info hover:!border-info focus:!border-info dark:border-navy-400"
+                                    name="outline_squircle" type="radio" />
+                                <span>Perempuan</span>
+                            </label>
+                        </div>
+                    </label>
                 </div>
                 <button
-                    class="btn mt-10 h-10 w-full bg-primary font-medium text-white hover:bg-primary-focus focus:bg-primary-focus active:bg-primary-focus/90 dark:bg-accent dark:hover:bg-accent-focus dark:focus:bg-accent-focus dark:active:bg-accent/90">
-                    Sign In
+                    class="btn mt-10 h-10 w-full bg-info font-medium text-white hover:bg-info-focus focus:bg-info-focus active:bg-info-focus/90 dark:bg-accent dark:hover:bg-accent-focus dark:focus:bg-accent-focus dark:active:bg-accent/90">
+                    Selanjutnya
                 </button>
                 <div class="mt-4 text-center text-xs+">
                     <p class="line-clamp-1">
-                        <span>Already have an account? </span>
-                        <a class="text-primary transition-colors hover:text-primary-focus dark:text-accent-light dark:hover:text-accent"
-                            href="pages-login-2.html">Sign In</a>
+                        <span>Sudah punya Akun? </span>
+                        <a class="text-info transition-colors hover:text-info-focus dark:text-accent-light dark:hover:text-accent"
+                            href="pages-login-2.html">Login</a>
                     </p>
                 </div>
             </div>
-        </main>
+    </div>
+    </main>
     </div>
 
     <!--
