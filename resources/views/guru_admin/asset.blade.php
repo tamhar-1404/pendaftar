7ce94bc7a70926e10242bcfab824d579ded69cd9
Kader2637 <kaderofficial33@gmail.com>
2023-06-20 12:57:01 +0700

    guru

--- resources/views/guru_admin/asset.blade.php
+++ resources/views/guru_admin/asset.blade.php
@@ -3585,7 +3585,7 @@
                       d="M5 19.111c0-2.413 1.697-4.468 4.004-4.848l.208-.035a17.134 17.134 0 015.576 0l.208.035c2.307.38 4.004 2.435 4.004 4.848C19 20.154 18.181 21 17.172 21H6.828C5.818 21 5 20.154 5 19.111zM16.083 6.938c0 2.174-1.828 3.937-4.083 3.937S7.917 9.112 7.917 6.937C7.917 4.764 9.745 3 12 3s4.083 1.763 4.083 3.938z"
                     />
                   </svg>
-                  <span>Profile</span>
+                  <a href="http://127.0.0.1:8000/detail">Profile</a>
                 </button>
                 <button
                   class="btn space-x-2 bg-slate-150 px-0 font-medium text-slate-800 hover:bg-slate-200 focus:bg-slate-200 active:bg-slate-200/80 dark:bg-navy-500 dark:text-navy-50 dark:hover:bg-navy-450 dark:focus:bg-navy-450 dark:active:bg-navy-450/90"
@@ -4505,7 +4505,7 @@
               </div>
             </div>
           </div>
-          
+
       </main>
     </div>
     <!--
