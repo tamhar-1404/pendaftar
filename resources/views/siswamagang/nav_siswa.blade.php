5bef3819e550cb66962d5d54b3cb05f7f74ae853
bicenginhere <127185668+bicenginhere@users.noreply.github.com>
2023-06-23 00:40:49 +0700

    eyejn
    eyihujoikpol[p

--- resources/views/siswamagang/nav_siswa.blade.php
+++ resources/views/siswamagang/nav_siswa.blade.php
@@ -416,7 +416,7 @@
                 </a>
             </li>
             <li class="menu nav-item relative">
-                <a href="javascript:;" class="nav-link">
+                <a href="{{ route('jurnalsiswa.index') }}" class="nav-link">
                     <div class="flex items-center">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
@@ -425,7 +425,7 @@
                     </div>
                 </a>
             </li>
-                <a href="javascript:;" class="nav-link">
+                <a href="{{ route('absensi_siswa.index') }}" class="nav-link">
                     <div class="flex items-center {{request()->routeIs('absensi.index') ? 'bg-blue-500 text-white' : ''}}">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25M9 16.5v.75m3-3v3M15 12v5.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
@@ -435,7 +435,7 @@
                 </a>
             </li>
             <li class="menu nav-item relative">
-                <a href="javascript:;" class="nav-link">
+                <a href="{{ route('berita_siswa.index') }}" class="nav-link">
                     <div class="flex items-center">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 01-2.25 2.25M16.5 7.5V18a2.25 2.25 0 002.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 002.25 2.25h13.5M6 7.5h3v3H6v-3z" />
@@ -491,13 +491,16 @@
                 </a>
                 <ul class="sub-menu">
                     <li>
-                        <a href="">Tata tertib</a>
+                        <a href="{{ route('tatib_siswa.index') }}">Tata tertib</a>
                     </li>
                     <li>
-                        <a href="">Jadwal Piket</a>
+                        <a href="{{ route('piket_siswa.index') }}">Jadwal Piket</a>
                     </li>
                     <li>
-                        <a href="">Chat</a>
+                        <a href="{{ route('chat_siswa.index') }}">Chat</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('siswa_magang.index') }}">Siswa</a>
                     </li>
                 </ul>
             </li>
