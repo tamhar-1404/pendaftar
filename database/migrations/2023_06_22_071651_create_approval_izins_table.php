ac18355b4c937cc13af87c0c895d485504f3674b
bicenginhere <127185668+bicenginhere@users.noreply.github.com>
2023-06-27 00:52:59 +0700

    dwefnjmk
    3erthy

--- database/migrations/2023_06_22_071651_create_approval_izins_table.php
+++ database/migrations/2023_06_22_071651_create_approval_izins_table.php
@@ -15,12 +15,14 @@ return new class extends Migration
     {
         Schema::create('approval_izins', function (Blueprint $table) {
             $table->id();
-            $table->string('kegiatan');
+            $table->string('nama');
+            $table->string('sekolah');
             $table->date('dari');
             $table->date('sampai');
-            $table->string('alamat');
             $table->string('keterangan');
+            $table->string('deskripsi');
             $table->string('bukti');
+            $table->string('status');
             $table->timestamps();
         });
     }
