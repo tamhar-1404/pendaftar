55be861c4759c0703f842b8bb5974842794a2593
bicenginhere <127185668+bicenginhere@users.noreply.github.com>
2023-07-06 22:13:22 +0700

    yuhj
    hj

--- app/Http/Controllers/AbsensiSiswaController.php
+++ app/Http/Controllers/AbsensiSiswaController.php
@@ -17,7 +17,7 @@ class AbsensiSiswaController extends Controller
      */
     public function index()
     {
-        $terima = ApprovalIzin::where('status', 'terimaabsen')->where('name', )
+        $terima = ApprovalIzin::where('status', 'terimaabsen')->where('nama', Auth::user('name') )
         ->get();
        return view('absensi_siswa.index' , compact('terima'));
     }
