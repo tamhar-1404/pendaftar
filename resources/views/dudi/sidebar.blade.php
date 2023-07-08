9184a1c7817168ab47198ccda04dbbff4c275c4d
Kader2637 <kaderofficial33@gmail.com>
2023-07-08 14:11:36 +0700

    grafik

--- resources/views/dudi/sidebar.blade.php
+++ resources/views/dudi/sidebar.blade.php
@@ -115,10 +115,44 @@
             </div>
         </div>
         {{-- grafik --}}
-        <div class="w-full  bg-white">
-            <div id="grafik_admin" class="relative  lg:w-[100%] md:w-[100%] xl:w-[100%] 2xl:w-[100%]" >
+        <div class="card col-span-12 lg:col-span-8">
+            <div
+              class="mt-3 flex flex-col justify-between px-4 sm:flex-row sm:items-center sm:px-5"
+            >
+              <div
+                class="flex flex-1 items-center justify-between space-x-2 sm:flex-initial"
+              >
 
-        </div>
+                <div
+                  x-data="usePopper({placement:'bottom-start',offset:4})"
+                  @click.outside="isShowPopper && (isShowPopper = false)"
+                  class="inline-flex"
+                >
+
+                  <div
+                    x-ref="popperRoot"
+                    class="popper-root"
+                    :class="isShowPopper && 'show'"
+                  >
+
+                  </div>
+                </div>
+              </div>
+              <div
+                class="hidden space-x-2 sm:flex"
+                x-data="{activeTab:'tabYearly'}"
+              >
+               
+              </div>
+            </div>
+
+
+            <div class="ax-transparent-gridline mt-2 px-2">
+              <div
+               id="grafik_admin"
+              ></div>
+            </div>
+          </div>
 
         </div>
 
