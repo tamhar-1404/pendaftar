ae05aa285c8f12364a0f59a93e006514a499cdee
femas <111944157+femas66@users.noreply.github.com>
2023-07-21 11:11:40 +0700

    fwea

--- resources/views/Berita/detail.blade.php
+++ resources/views/Berita/detail.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Admin - Detail Berita</title>
-    @vite('resources/css/app.css')
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tw-elements/dist/css/tw-elements.min.css" />
     <script src="https://cdn.tailwindcss.com/3.3.0"></script>
