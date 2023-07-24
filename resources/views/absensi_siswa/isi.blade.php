d71c78833f74e1c2382d979b84dd75bd495bf348
femas <femasakbar69@gmail.com>
2023-07-24 10:01:01 +0700

    btn kembali modal detail absen

--- resources/views/absensi_siswa/isi.blade.php
+++ resources/views/absensi_siswa/isi.blade.php
@@ -505,7 +505,7 @@
                     <!-- Modal footer -->
                     <div
                         class="flex items-center justify-end p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-                        <button data-modal-hide="staticModal"
+                        <button data-modal-hide="staticModal1{{$absen->id}}"
                             class="text-gray-700  bg-white border border-gray-700 hover:text-white hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Kembali</button>
                     </div>
                 </div>
