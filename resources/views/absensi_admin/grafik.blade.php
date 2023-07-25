1b87107f44d0406d3b0c4e99bb0a55848f106553
Kader <130645424+Kader2637@users.noreply.github.com>
2023-07-25 09:33:31 +0700

    push

--- resources/views/absensi_admin/grafik.blade.php
+++ resources/views/absensi_admin/grafik.blade.php
@@ -299,8 +299,8 @@
                             <div class="bg-red-100 border mb-2 mt-2 border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
                                               <strong class="font-bold">Data </strong>
                                               <span class="block sm:inline">Tidak tersedia.</span>
-                                              <span class="absolute top-0 bottom-0 right-0 px-4 py-3"></span>
-                                             </div>x
+
+                                             </div>
                             @endforelse
                         </tbody>
                       </table>
