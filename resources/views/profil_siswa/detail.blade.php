3de296f1fa24a5d603ffe5ef7135fab664bcf3b3
tamhar-1404 <127166391+tamhar-1404@users.noreply.github.com>
2023-08-01 10:05:28 +0700

    oke

--- resources/views/profil_siswa/detail.blade.php
+++ resources/views/profil_siswa/detail.blade.php
@@ -1396,8 +1396,9 @@
                                     @endforelse
 
                                     @forelse ($data as $pelangaran)
+                                    <div class="w-full h-screen pt-[100px]">
                                         <div id="staticModal{{ $pelangaran->id }}" tabindex="-1"
-                                            aria-hidden="true"
+                                            aria-hidden="true" style="backdrop-filter: blur(4px);"
                                             class="kamu-tak-diajak fixed top-3 left-0 right-0 z-[999] hidden w-120 p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
                                             <div class="relative w-full max-w-xl max-h-full">
                                                 <!-- Modal content -->
@@ -1459,6 +1460,7 @@
                                                 </div>
                                             </div>
                                         </div>
+                                    </div>
                                     @empty
                                     @endforelse
 
