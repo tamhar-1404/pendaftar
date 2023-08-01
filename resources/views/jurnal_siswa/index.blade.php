97ebb1807b7b2ffe3483760b479a91e1aaa28bee
tamhar-1404 <127166391+tamhar-1404@users.noreply.github.com>
2023-08-01 10:05:40 +0700

    p

--- resources/views/jurnal_siswa/index.blade.php
+++ resources/views/jurnal_siswa/index.blade.php
@@ -1158,7 +1158,6 @@
                     }
                 },
 
-                        image: '<span class="grid place-content-center w-9 h-9 rounded-full bg-danger-light dark:bg-danger text-danger dark:text-danger-light"><svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></span>',
 
                 removeNotification(value) {
                     this.notifications = this.notifications.filter((d) => d.id !== value);
