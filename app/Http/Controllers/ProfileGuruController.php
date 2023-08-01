e6a68371fea3dbd0afdc3aef89bccd4668e5ef54
femas <femasakbar69@gmail.com>
2023-08-01 22:34:43 +0700

    validasi unik

--- app/Http/Controllers/ProfileGuruController.php
+++ app/Http/Controllers/ProfileGuruController.php
@@ -88,7 +88,7 @@ class ProfileGuruController extends Controller
         if ($request->has('image')) {
             $request->validate([
                 'image' => 'required|image|mimes:png,jpg,jpeg',
-                'email' => 'required|email|unique:users,email,' . auth()->user()->id . '|unique:guru_admins,email,' . $guru->id,
+                'email' => 'required|email|unique:users,email,' . auth()->user()->id,
                 'no' => 'required',
                 'alamat' => 'required',
             ]);
@@ -111,7 +111,7 @@ class ProfileGuruController extends Controller
         }
         else {
             $request->validate([
-                'email' => 'required|email|unique:users,email,' . auth()->user()->id . '|unique:guru_admins,email,' . $guru->id,
+                'email' => 'required|email|unique:users,email,' . auth()->user()->id,
                 'no' => 'required',
                 'alamat' => 'required',
             ]);
