97dc5999619bceab2dcad725d7d3d72e2d5f936d
tamhar-1404 <127166391+tamhar-1404@users.noreply.github.com>
2023-08-03 14:16:27 +0700

    oe\

--- resources/views/piket_siswa/index.blade.php
+++ resources/views/piket_siswa/index.blade.php
@@ -64,6 +64,11 @@
         toastr.success("{{ session('success') }}");
     </script>
 @endif
+    @if (session('error'))
+    <script>
+        toastr.error("{{ session('error') }}");
+    </script>
+@endif
     <!-- sidebar menu overlay -->
     <div x-cloak class="fixed inset-0 z-50 bg-[black]/60 lg:hidden" :class="{ 'hidden': !$store.app.sidebar }"
         @click="$store.app.toggleSidebar()"></div>
