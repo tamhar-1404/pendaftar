cee95aab8febd02f66601e68e3d3e53ebb285213
femas <femasakbar69@gmail.com>
2023-08-03 09:02:03 +0700

    fwewfea

--- app/Http/Controllers/SpController.php
+++ app/Http/Controllers/SpController.php
@@ -23,7 +23,7 @@ class SpController extends Controller
     {
         if ($request->has('cari')) {
             $keyword = $request->cari;
-            $data = Sp::where('nama', 'LIKE', '%' . $keyword . '%')->orWhere('deskripsi', 'LIKE', '%' . $keyword . '%')->paginate(10);
+            $data = Sp::where('nama', 'LIKE', '%' . $keyword . '%')->orWhere('deskripsi_1', 'LIKE', '%' . $keyword . '%')->orWhere('deskripsi_2', 'LIKE', '%' . $keyword . '%')->paginate(10);
             return view('sp.index', compact('data'));
 
             $data->appends(['cari' => $keyword]);
