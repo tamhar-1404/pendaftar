b2a5f1376b754a43f406419d8ec3a280efc61012
Kader <130645424+Kader2637@users.noreply.github.com>
2023-08-06 22:46:20 +0700

    push

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -7,12 +7,9 @@
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 <head>
-    <!-- Meta tags  -->
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
-
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
     <title>Login</title>
     <script src="https://cdn.tailwindcss.com%22%3E/"></script>
     <script>
@@ -25,23 +22,15 @@
         }
     </script>
     <link href="/admin/assets/images/Logo.png" rel="shortcut icon">
-
-    <!-- CSS Assets -->
     <link rel="stylesheet" href="lineone/css/app.css" />
-
-    <!-- Javascript Assets -->
     <script src="lineone/js/app.js" defer></script>
-
-    <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com/" />
     <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin />
-    <link
-        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&amp;family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&amp;display=swap"
-        rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&amp;family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&amp;display=swap" rel="stylesheet" />
     <script>
         /**
-         * THIS SCRIPT REQUIRED FOR PREVENT FLICKERING IN SOME BROWSERS
-         */
+        * THIS SCRIPT REQUIRED FOR PREVENT FLICKERING IN SOME BROWSERS
+        */
         localStorage.getItem("_x_darkMode_on") === "true" &&
             document.documentElement.classList.add("dark");
     </script>
