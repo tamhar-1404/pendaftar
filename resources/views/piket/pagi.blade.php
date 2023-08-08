85e50d796ddad9fab5b8a0a6a8a3ec0930df3b75
Kader <130645424+Kader2637@users.noreply.github.com>
2023-08-08 08:24:15 +0700

    push

--- resources/views/piket/pagi.blade.php
+++ resources/views/piket/pagi.blade.php
@@ -1377,71 +1377,48 @@
                                 </div>
                             </div>
                             {{-- step 3 --}}
-                            <div id="content3" class="card-content" style="display: none">
-                                <div class="flex grid-cols-3 gap-2 w-lg p-1">
+                            <div id="content3" class="card-content hidden">
+                                <div class="grid grid-cols-1 md:grid-cols-3 gap-4 p-4">
                                     @forelse ($laporan_piket as $data)
-                                        <div class="w-1/3 h-56 bg-white drop-shadow ">
-                                            <div class="flex ml-5 gap-3 mt-3">
+                                        <div class="w-full md:w-1/3 bg-white shadow-md rounded-lg">
+                                            <div class="flex items-center p-4">
                                                 @foreach ($siswa as $sis)
                                                     @if ($sis->name == $data->name)
-                                                    <img src="{{ asset('storage/Siswa/' . $sis->foto_siswa) }}"
-                                                    class="w-14 rounded-full" alt="" srcset="">
+                                                        <img src="{{ asset('storage/Siswa/' . $sis->foto_siswa) }}" class="w-14 h-14 rounded-full" alt="{{ $sis->name }}">
                                                     @endif
                                                 @endforeach
-                                                <div class=" font-normal mt-1">
-                                                    <div class="font-bold"> {{ $data->name }}</div>
-                                                    {{ $data->created_at }}
+                                                <div class="ml-3">
+                                                    <div class="font-bold">{{ $data->name }}</div>
+                                                    <div>{{ $data->created_at }}</div>
                                                 </div>
                                             </div>
-                                            <div>
-                                                <div class=" font-bold ml-7 mt-5"> {{ $data->waktu }}</div>
-                                                <div class="truncate px-7 mt-3">
-                                                    {{ $data->deskripsi }}
-                                                </div>
-                                                <div class="flex justify-between px-7 mt-6">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                        viewBox="0 0 24 24" stroke-width="1.5"
-                                                        stroke="currentColor" class="w-6 h-6"
-                                                        data-modal-target="authentication-modal{{ $data->id }}"
-                                                        data-modal-toggle="authentication-modal{{ $data->id }}">
-                                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                                            d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
-                                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                                            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                            <div class="p-4">
+                                                <div class="font-bold">{{ $data->waktu }}</div>
+                                                <div class="truncate mt-2">{{ $data->deskripsi }}</div>
+                                                <div class="flex justify-between mt-4">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6 text-blue-500 cursor-pointer" data-modal-target="authentication-modal{{ $data->id }}" data-modal-toggle="authentication-modal{{ $data->id }}">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                                                     </svg>
-                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                        viewBox="0 0 24 24" stroke-width="1.5"
-                                                        stroke="currentColor" class="w-6 h-6">
-                                                        <path stroke-linecap="round" color="red"
-                                                            stroke-linejoin="round"
-                                                            d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6 text-red-500 cursor-pointer">
+                                                        <path stroke-linecap="round" color="red" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                                                     </svg>
                                                 </div>
                                             </div>
                                         </div>
                                         <!-- Main modal -->
-                                        <div id="authentication-modal{{ $data->id }}" tabindex="-1"
-                                            aria-hidden="true"
-                                            class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
-                                            <div class="relative w-full max-w-md max-h-full">
+                                        <div id="authentication-modal{{ $data->id }}" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto inset-0 md:h-auto">
+                                            <div class="relative w-full max-w-md mx-auto">
                                                 <!-- Modal content -->
-                                                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                                                    <button type="button"
-                                                        class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
-                                                        data-modal-hide="authentication-modal{{ $data->id }}">
-                                                        <svg aria-hidden="true" class="w-5 h-5"
-                                                            fill="currentColor" viewBox="0 0 20 20"
-                                                            xmlns="http://www.w3.org/2000/svg">
-                                                            <path fill-rule="evenodd"
-                                                                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                                                clip-rule="evenodd"></path>
+                                                <div class="relative bg-white rounded-lg shadow-md dark:bg-gray-700">
+                                                    <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="authentication-modal{{ $data->id }}">
+                                                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                                            <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
                                                         </svg>
                                                         <span class="sr-only">Close modal</span>
                                                     </button>
                                                     <div class="px-6 py-6 lg:px-8">
-                                                        <h3
-                                                            class="mb-4 text-xl font-medium text-gray-900 dark:text-white">
-                                                            Laporan Piket</h3>
+                                                        <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Laporan Piket</h3>
                                                         <div class="text-md font-normal">
                                                             {{ $data->deskripsi }}
                                                         </div>
@@ -1453,12 +1430,7 @@
                                     @endforelse
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+
     {{-- <script>
         document.addEventListener("DOMContentLoaded", function() {
           const form = document.getElementById("wizardForm");
