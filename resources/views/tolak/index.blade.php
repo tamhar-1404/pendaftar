f8c15708e2451a37ad67a14072040c47a3367ab8
tamhar-1404 <127166391+tamhar-1404@users.noreply.github.com>
2023-08-09 11:09:28 +0700

    ansj

--- resources/views/tolak/index.blade.php
+++ resources/views/tolak/index.blade.php
@@ -6,95 +6,60 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Admin - Tolak</title>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"
-        integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
-    <script>
-        theme: {
-            extend: {
-                blur: {
-                    xs: '3px',
-                }
-            },
-        },
-    </script>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" />
-    <link href="/admin/assets/images/Logo.png" rel="shortcut icon">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&display=swap" rel="stylesheet" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tw-elements/dist/css/tw-elements.min.css" />
-    <script src="https://cdn.tailwindcss.com/3.3.0"></script>
-    <link rel="stylesheet" href="load/load.css">
-    <link href="/admin/assets/images/logo.png" rel="shortcut icon">
-    <link rel="preconnect" href="https://fonts.googleapis.com/" />
-    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;600;700;800&amp;display=swap"
-        rel="stylesheet" />
-    <link rel="stylesheet" type="text/css" media="screen" href="assets_guru/css/perfect-scrollbar.min.css" />
-    <link rel="stylesheet" type="text/css" media="screen" href="assets_guru/css/style.css" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tw-elements/dist/css/tw-elements.min.css" />
-    <link defer rel="stylesheet" type="text/css" media="screen" href="assets_guru/css/animate.css" />
-    {{--  <link defer rel="stylesheet" type="text/css" media="screen" href="{{ asset('admin/assets/css/animate.css') }}" />  --}}
-    <script src="{{ asset('admin/assets/js/perfect-scrollbar.min.js') }}"></script>
-    <script defer src="{{ asset('admin/assets/js/popper.min.js') }}"></script>
-    <link rel="stylesheet" type="text/css" media="screen" href="{{ asset('admin/assets/css/style.css') }}" />
-    <script defer src="{{ asset('admin/assets/js/tippy-bundle.umd.min.js') }}"></script>
-    <script defer src="{{ asset('admin/assets/js/sweetalert.min.js') }}"></script>
-    <link rel="stylesheet" href="load/load.css">
-    <!-- CSS Assets -->
-    <link rel="stylesheet" href="admin/css/app.css" />
+    <!DOCTYPE html>
+    <html lang="en">
 
-    <!-- Javascript Assets -->
-    <script src="admin/js/app.js" defer></script>
-    <script>
-        tailwind.config = {
-            darkMode: "class",
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Admin - Data Ditolak</title>
+        <script src="https://cdn.tailwindcss.com%22%3E/"></script>
+        <script>
             theme: {
-                fontFamily: {
-                    sans: ["Roboto", "sans-serif"],
-                    body: ["Roboto", "sans-serif"],
-                    mono: ["ui-monospace", "monospace"],
+                extend: {
+                    blur: {
+                        xs: '3px',
+                    }
                 },
             },
-            corePlugins: {
-                preflight: false,
-            },
-        };
-    </script>
-    <script src="assets_guru/js/perfect-scrollbar.min.js"></script>
-    <script defer src="assets_guru/js/popper.min.js"></script>
-    <script defer src="assets_guru/js/tippy-bundle.umd.min.js"></script>
-    <script defer src="assets_guru/js/sweetalert.min.js"></script>
-    <style>
-        /* Gaya untuk overlay popup */
-        .popup-overlay {
-          display: none;
-          position: fixed;
-          top: 0;
-          left: 0;
-          width: 100%;
-          height: 100%;
-          background-color: rgba(0, 0, 0, 0.5);
-          z-index: 9999;
-        }
+        </script>
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&display=swap" rel="stylesheet" />
+        <link href="/admin/assets/images/Logo.png" rel="shortcut icon">
+        <link rel="preconnect" href="https://fonts.googleapis.com/" />
+        <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;600;700;800&amp;display=swap"
+            rel="stylesheet" />
+        <link rel="stylesheet" type="text/css" media="screen"
+            href="{{ asset('admin/assets/css/perfect-scrollbar.min.css') }}" />
+        <link rel="stylesheet" type="text/css" media="screen" href="{{ asset('admin/assets/css/style.css') }}" />
+        <link rel="stylesheet" href="load/load.css">
+        <link rel="stylesheet" href="assets/css/swiper-bundle.min.css" />
+        {{--  <link defer rel="stylesheet" type="text/css" media="screen" href="{{ asset('admin/assets/css/animate.css') }}" />  --}}
+        <script src="{{ asset('admin/assets/js/perfect-scrollbar.min.js') }}"></script>
+        <script defer src="{{ asset('admin/assets/js/popper.min.js') }}"></script>
+        <script defer src="{{ asset('admin/assets/js/tippy-bundle.umd.min.js') }}"></script>
+        <script defer src="{{ asset('admin/assets/js/sweetalert.min.js') }}"></script>
+        {{--  <!-- Javascript Assets -->  --}}
+        <script src="admin/js/app.js" defer></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tw-elements/dist/css/tw-elements.min.css" />
+        <script src="https://cdn.tailwindcss.com/3.3.0"></script>
+        <script>
+            tailwind.config = {
+                darkMode: "class",
+                theme: {
+                    fontFamily: {
+                        sans: ["Roboto", "sans-serif"],
+                        body: ["Roboto", "sans-serif"],
+                        mono: ["ui-monospace", "monospace"],
+                    },
+                },
+                corePlugins: {
+                    preflight: false,
+                },
+            };
+        </script>
 
-        /* Gaya untuk konten popup */
-        .popup-content {
-          display: none;
-          position: absolute;
-          top: 50%;
-          left: 50%;
-          transform: translate(-50%, -50%);
-          background-color: #fff;
-          padding: 20px;
-          border-radius: 5px;
-          box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
-        }
-      </style>
 </head>
 
 <body>
@@ -628,227 +593,12 @@
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
 
-<script src="assets_guru/js/alpine-collaspe.min.js"></script>
-<script src="assets_guru/js/alpine-persist.min.js"></script>
-<script defer src="assets_guru/js/alpine-ui.min.js"></script>
-<script defer src="assets_guru/js/alpine-focus.min.js"></script>
+
 <script defer src="assets_guru/js/alpine.min.js"></script>
-<script src="assets_guru/js/custom.js"></script>
 
 
-<script>
-    document.addEventListener('alpine:init', () => {
-        // main section
-        Alpine.data('scrollToTop', () => ({
-            showTopButton: false,
-            init() {
-                window.onscroll = () => {
-                    this.scrollFunction();
-                };
-            },
 
-            scrollFunction() {
-                if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-                    this.showTopButton = true;
-                } else {
-                    this.showTopButton = false;
-                }
-            },
 
-            goToTop() {
-                document.body.scrollTop = 0;
-                document.documentElement.scrollTop = 0;
-            },
-        }));
-
-        // theme customization
-        Alpine.data('customizer', () => ({
-            showCustomizer: false,
-        }));
-
-        // sidebar section
-        Alpine.data('sidebar', () => ({
-            init() {
-                const selector = document.querySelector('.sidebar ul a[href="' + window.location
-                    .pathname + '"]');
-                if (selector) {
-                    selector.classList.add('active');
-                    const ul = selector.closest('ul.sub-menu');
-                    if (ul) {
-                        let ele = ul.closest('li.menu').querySelectorAll('.nav-link');
-                        if (ele) {
-                            ele = ele[0];
-                            setTimeout(() => {
-                                ele.click();
-                            });
-                        }
-                    }
-                }
-            },
-        }));
-
-        // header section
-        Alpine.data('header', () => ({
-            init() {
-                const selector = document.querySelector('ul.horizontal-menu a[href="' + window
-                    .location.pathname + '"]');
-                if (selector) {
-                    selector.classList.add('active');
-                    const ul = selector.closest('ul.sub-menu');
-                    if (ul) {
-                        let ele = ul.closest('li.menu').querySelectorAll('.nav-link');
-                        if (ele) {
-                            ele = ele[0];
-                            setTimeout(() => {
-                                ele.classList.add('active');
-                            });
-                        }
-                    }
-                }
-            },
-
-            notifications: [{
-                    id: 1,
-                    profile: 'user-profile.jpeg',
-                    message: '<strong class="text-sm mr-1">John Doe</strong>invite you to <strong>Prototyping</strong>',
-                    time: '45 min ago',
-                },
-                {
-                    id: 2,
-                    profile: 'profile-34.jpeg',
-                    message: '<strong class="text-sm mr-1">Adam Nolan</strong>mentioned you to <strong>UX Basics</strong>',
-                    time: '9h Ago',
-                },
-                {
-                    id: 3,
-                    profile: 'profile-16.jpeg',
-                    message: '<strong class="text-sm mr-1">Anna Morgan</strong>Upload a file',
-                    time: '9h Ago',
-                },
-            ],
-
-            messages: [{
-                    id: 1,
-                    image: '<span class="grid place-content-center w-9 h-9 rounded-full bg-success-light dark:bg-success text-success dark:text-success-light"><svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path></svg></span>',
-                    title: 'Congratulations!',
-                    message: 'Your OS has been updated.',
-                    time: '1hr',
-                },
-                {
-                    id: 2,
-                    image: '<span class="grid place-content-center w-9 h-9 rounded-full bg-info-light dark:bg-info text-info dark:text-info-light"><svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="16" x2="12" y2="12"></line><line x1="12" y1="8" x2="12.01" y2="8"></line></svg></span>',
-                    title: 'Did you know?',
-                    message: 'You can switch between artboards.',
-                    time: '2hr',
-                },
-                {
-                    id: 3,
-                    image: '<span class="grid place-content-center w-9 h-9 rounded-full bg-danger-light dark:bg-danger text-danger dark:text-danger-light"><svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></span>',
-                    title: 'Something went wrong!',
-                    message: 'Send Reposrt',
-                    time: '2days',
-                },
-                {
-                    id: 4,
-                    image: '<span class="grid place-content-center w-9 h-9 rounded-full bg-warning-light dark:bg-warning text-warning dark:text-warning-light"><svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">    <circle cx="12" cy="12" r="10"></circle>    <line x1="12" y1="8" x2="12" y2="12"></line>    <line x1="12" y1="16" x2="12.01" y2="16"></line></svg></span>',
-                    title: 'Warning',
-                    message: 'Your password strength is low.',
-                    time: '5days',
-                },
-            ],
-
-            languages: [{
-                    id: 1,
-                    key: 'Chinese',
-                    value: 'zh',
-                },
-                {
-                    id: 2,
-                    key: 'Danish',
-                    value: 'da',
-                },
-                {
-                    id: 3,
-                    key: 'English',
-                    value: 'en',
-                },
-                {
-                    id: 4,
-                    key: 'French',
-                    value: 'fr',
-                },
-                {
-                    id: 5,
-                    key: 'German',
-                    value: 'de',
-                },
-                {
-                    id: 6,
-                    key: 'Greek',
-                    value: 'el',
-                },
-                {
-                    id: 7,
-                    key: 'Hungarian',
-                    value: 'hu',
-                },
-                {
-                    id: 8,
-                    key: 'Italian',
-                    value: 'it',
-                },
-                {
-                    id: 9,
-                    key: 'Japanese',
-                    value: 'ja',
-                },
-                {
-                    id: 10,
-                    key: 'Polish',
-                    value: 'pl',
-                },
-                {
-                    id: 11,
-                    key: 'Portuguese',
-                    value: 'pt',
-                },
-                {
-                    id: 12,
-                    key: 'Russian',
-                    value: 'ru',
-                },
-                {
-                    id: 13,
-                    key: 'Spanish',
-                    value: 'es',
-                },
-                {
-                    id: 14,
-                    key: 'Swedish',
-                    value: 'sv',
-                },
-                {
-                    id: 15,
-                    key: 'Turkish',
-                    value: 'tr',
-                },
-            ],
-
-            removeNotification(value) {
-                this.notifications = this.notifications.filter((d) => d.id !== value);
-            },
-
-            removeMessage(value) {
-                this.messages = this.messages.filter((d) => d.id !== value);
-            },
-        }));
-    });
-</script>
-<script>
-    $(window).on('load', function() {
-        $('.spin_load').fadeOut();
-    });
-</script>
 
 
 </div>
@@ -862,48 +612,8 @@
         $('.spin_load').fadeOut();
     });
 </script>
-<script>
-    const instanceMode = te.Sidenav.getInstance(
-        document.getElementById("sidenav-2")
-    );
-    const modes = ["side"];
 
-    modes.forEach((mode) => {
-        const modeSwitch = document.getElementById(mode);
-        modeSwitch.addEventListener("click", () => {
-            const instance = te.Sidenav.getInstance(
-                document.getElementById("sidenav-2")
-            );
-            instance.changeMode(mode);
-            modes.forEach((el) => {
-                if (el === mode) {
-                    ["text-primary-600", "border-primary-600"].forEach((item) =>
-                        modeSwitch.classList.remove(item)
-                    );
-                    modeSwitch.className +=
-                        " bg-primary text-white hover:bg-primary-700 active:bg-primary-800 focus:bg-primary-700 border-transparent";
-                } else {
-                    const node = document.getElementById(el);
-                    node.className += " text-primary-600 border-primary-600";
-                    [
 
-                    ].forEach((item) => node.classList.remove(item));
-                }
-            });
-        });
-    });
-</script>
-<script>
-    // Initialization for ES Users
-    import {
-        sidenav - 2,
-        initTE,
-    } from "tw-elements";
-
-    initTE({
-        sidenav - 2
-    });
-</script>
 <script>
     function openModal(id) {
         $(`#staticModal${id}`).show();
