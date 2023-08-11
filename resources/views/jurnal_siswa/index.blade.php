be5d37c5b364dfe5ee17fa1625f25aa1d4e242f9
femas <femasakbar69@gmail.com>
2023-08-11 10:41:09 +0700

    wkwkw

--- resources/views/jurnal_siswa/index.blade.php
+++ resources/views/jurnal_siswa/index.blade.php
@@ -484,23 +484,6 @@
                                 <div class="kamu-tak-diajak flex justify-start items-center">
                                     {{-- serch --}}
                                     <div class="mr-4 kamu-tak-diajak">
-                                        <form action="" class="kamu-tak-diajak">
-                                            <label class="relative hidden sm:flex kamu-tak-diajak">
-                                                <input
-                                                    class="kamu-tak-diajak form-input peer h-9 w-full rounded-full border border-slate-300 bg-transparent px-3 py-2 pl-9 text-xs+ placeholder:text-slate-400/70 hover:border-slate-400 focus:border-primary dark:border-navy-450 dark:hover:border-navy-400 dark:focus:border-accent"
-                                                    placeholder="Search users..." type="text" name="cari"
-                                                    value="{{ request('cari') }}" />
-                                                <span
-                                                    class=" pointer-events-none absolute flex h-full w-10 items-center justify-center text-slate-400 peer-focus:text-primary dark:text-navy-300 dark:peer-focus:text-accent kamu-tak-diajak">
-                                                    <svg xmlns="http://www.w3.org/2000/svg"
-                                                        class="h-4 w-4 transition-colors duration-200 kamu-tak-diajak"
-                                                        fill="currentColor" viewBox="0 0 24 24">
-                                                        <path class="kamu-tak-diajak"
-                                                            d="M3.316 13.781l.73-.171-.73.171zm0-5.457l.73.171-.73-.171zm15.473 0l.73-.171-.73.171zm0 5.457l.73.171-.73-.171zm-5.008 5.008l-.171-.73.171.73zm-5.457 0l-.171.73.171-.73zm0-15.473l-.171-.73.171.73zm5.457 0l.171-.73-.171.73zM20.47 21.53a.75.75 0 101.06-1.06l-1.06 1.06zM4.046 13.61a11.198 11.198 0 010-5.115l-1.46-.342a12.698 12.698 0 000 5.8l1.46-.343zm14.013-5.115a11.196 11.196 0 010 5.115l1.46.342a12.698 12.698 0 000-5.8l-1.46.343zm-4.45 9.564a11.196 11.196 0 01-5.114 0l-.342 1.46c1.907.448 3.892.448 5.8 0l-.343-1.46zM8.496 4.046a11.198 11.198 0 015.115 0l.342-1.46a12.698 12.698 0 00-5.8 0l.343 1.46zm0 14.013a5.97 5.97 0 01-4.45-4.45l-1.46.343a7.47 7.47 0 005.568 5.568l.342-1.46zm5.457 1.46a7.47 7.47 0 005.568-5.567l-1.46-.342a5.97 5.97 0 01-4.45 4.45l.342 1.46zM13.61 4.046a5.97 5.97 0 014.45 4.45l1.46-.343a7.47 7.47 0 00-5.568-5.567l-.342 1.46zm-5.457-1.46a7.47 7.47 0 00-5.567 5.567l1.46.342a5.97 5.97 0 014.45-4.45l-.343-1.46zm8.652 15.28l3.665 3.664 1.06-1.06-3.665-3.665-1.06 1.06z" />
-                                                    </svg>
-                                                </span>
-                                            </label>
-                                        </form>
                                     </div>
                                     {{-- filter --}}
                                 </div>
@@ -615,14 +598,14 @@
                                                             </div>
                                                         </td>
                                                         <td class="whitespace-nowrap px-6 pl-17 py-2">
-                                                            @if ($items->kegiatan != 'Tidak mengisi' || $items->kegiatan != 'Kosong')
+                                                            @if ($items->kegiatan != 'Tidak mengisi' && $items->kegiatan != 'Kosong')
                                                             <img
                                                                 src="{{ asset('storage/image/' . $items->image) }}"
                                                                 width="50px" height="50px" alt="{{ $items->image }}">
                                                             @endif
                                                         </td>
                                                         <td class="whitespace-nowrap px-6 py-2  kamu-tak-diajak">
-                                                            @if ($items->kegiatan != 'Tidak mengisi' || $items->kegiatan != 'Kosong')
+                                                            @if ($items->kegiatan != 'Tidak mengisi' && $items->kegiatan != 'Kosong')
                                                             <div class="flex gap-2">
                                                                 <a href="#">
                                                                     <div class="w-10 flex h-8 bg-white rounded-md border-2 border-[#00B7FF] justify-center items-center text-[#00B7FF] hover:bg-[#00B7FF] hover:text-white dark:bg-transparent"
