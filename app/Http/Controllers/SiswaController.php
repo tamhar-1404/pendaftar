631eda835f87df03b20a2d7cda2fbf5399a8187b
Femas Akbar Faturrohim <111944157+femas66@users.noreply.github.com>
2023-12-07 15:09:58 +0700

    update

--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -246,7 +246,7 @@ return view('rfid.index', compact('users'));
     public function unban(Siswa $student) : RedirectResponse
     {
         $student->update(['status' => "", 'role' => 'siswa']);
-        $student->user->update(['role' => 'Siswa']);
+        $student->user()->update(['role' => 'Siswa']);
         return back()->with('success', 'Berhasil memperbarui data');
     }
 
