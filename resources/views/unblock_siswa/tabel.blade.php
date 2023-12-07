97a8bae548515f8888294b4fd587c2f2b84edbd1
Kader2637 <kadergantengrek@gmail.com>
2023-12-07 15:36:45 +0700

    Update tabel.blade.php

--- resources/views/unblock_siswa/tabel.blade.php
+++ resources/views/unblock_siswa/tabel.blade.php
@@ -73,3 +73,12 @@
     });
   }
 </script>
+@if(session('success'))
+<script>
+    Swal.fire({
+        icon: 'success',
+        title: 'Success',
+        text: '{{ session('success') }}',
+    });
+</script>
+@endif
