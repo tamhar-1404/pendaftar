4614514480a70adf81e395f228c1c0ab3d7b93e3
rahmat <rahmatmahendra888@gmail.com>
2023-12-15 09:46:45 +0700

    Update ReportController.php

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -4,13 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Models\Siswa;
 use App\MonthEnum;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ReportController extends Controller
 {
     public function index()  {
-        $data = Siswa::with('jurnals','absens')->get();
-        // dd($data);
+        $date = Carbon::now();
+        $month = $date->format('m');
+        $data = Siswa::with(['jurnals' => function ($query) use ($month) {
+            $query->whereMonth('created_at', $month);
+        }, 'absens' => function ($query) use ($month) {
+            $query->whereMonth('created_at', $month);
+        }])->get();
         return view('report.index', compact('data'));
     }
 }
