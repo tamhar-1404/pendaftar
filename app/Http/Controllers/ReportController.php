6f5f266e572a80922a040b7ba840cc44bab5434c
rahmat <rahmatmahendra888@gmail.com>
2023-12-15 10:32:47 +0700

    report

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -9,14 +9,21 @@ use Illuminate\Http\Request;
 
 class ReportController extends Controller
 {
-    public function index()  {
+    public function index(Request $request)  {
         $date = Carbon::now();
         $month = $date->format('m');
+        $MonthYear = $date->format('Y-m');
+        if($request->has('month')){
+            $MonthYear = \DateTime::createFromFormat('Y-m',$request->input('month'));
+            $month = $MonthYear->format('m');
+            $MonthYear = $MonthYear->format('Y-m');
+        }
+        // dd($month);
         $data = Siswa::with(['jurnals' => function ($query) use ($month) {
             $query->whereMonth('created_at', $month)->where('status', 'Tidak mengisi');
         }, 'absens' => function ($query) use ($month) {
             $query->whereMonth('created_at', $month)->whereIn('status', ['telat','alfa']);
         }])->orderBy('name', 'asc')->paginate(15);
-        return view('report.index', compact('data'));
+        return view('report.index', compact('data','MonthYear'));
     }
 }
