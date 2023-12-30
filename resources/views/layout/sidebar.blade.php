c4530ed6c06d92bd93dfa4b3157e25dab3285e16
Kader2637 <kadergantengrek@gmail.com>
2023-12-30 16:06:03 +0700

    Update sidebar.blade.php

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -8,7 +8,7 @@
             <ul class="metismenu list-unstyled" id="side-menu">
                 <li class="menu-title" key="t-menu">Menu</li>
                 <li>
-                    <a href="chat.html" class="waves-effect">
+                    <a href="master" class="waves-effect">
                         <i class="bx bx-home-circle"></i>
                         <span key="t-chat">Dashboard</span>
                     </a>
