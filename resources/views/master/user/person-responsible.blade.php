9809bee24d78a2a2ab11acdb8cee1ee87e95ac03
Femas Akbar Faturrohim <111944157+femas66@users.noreply.github.com>
2023-12-31 07:05:35 +0700

    forelse

--- resources/views/master/user/person-responsible.blade.php
+++ resources/views/master/user/person-responsible.blade.php
@@ -3,22 +3,23 @@
 <div class="row">
     <div class="col-12">
         <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-            <h4 class="mb-sm-0 font-size-18">Data Siswa</h4>
+            <h4 class="mb-sm-0 font-size-18">Data Penanggung Jawab</h4>
         </div>
     </div>
 </div>
 
 <div class="row">
+    @foreach ($teachers as $teacher)
     <div class="col-xl-3 col-sm-6">
         <div class="card text-center">
             <div class="card-body">
                 <div class="avatar-sm mx-auto mb-4">
-                    <span class="avatar-title rounded-circle bg-warning-subtle text-warning font-size-16">
-                        D
-                    </span>
+                    <div class="avatar-sm mx-auto mb-4">
+                        <img class="avatar-title rounded-circle bg-primary-subtle text-primary " src="{{ asset('storage/guru_image/' . $teacher->foto_siswa) }}">
+                    </div>
                 </div>
-                <h5 class="font-size-15 mb-1"><a href="javascript: void(0);" class="text-dark">David McHenry</a></h5>
-                <p class="text-muted">Smkn 1 kraksaan</p>
+                <h5 class="font-size-15 mb-1"><a href="javascript: void(0);" class="text-dark">{{ $teacher->name }}</a></h5>
+                <p class="text-muted">{{ $teacher->sekolah }}</p>
                 <div>
                     <a href="javascript: void(0);" class="badge bg-warning font-size-11 m-1">Penanggung Jawab</a>
                 </div>
@@ -32,259 +33,7 @@
             </div>
         </div>
     </div>
-    <div class="col-xl-3 col-sm-6">
-        <div class="card text-center">
-            <div class="card-body">
-                <div class="avatar-sm mx-auto mb-4">
-                    <span class="avatar-title rounded-circle bg-warning-subtle text-warning font-size-16">
-                        D
-                    </span>
-                </div>
-                <h5 class="font-size-15 mb-1"><a href="javascript: void(0);" class="text-dark">David McHenry</a></h5>
-                <p class="text-muted">Smkn 1 kraksaan</p>
-                <div>
-                    <a href="javascript: void(0);" class="badge bg-warning font-size-11 m-1">Penanggung Jawab</a>
-                </div>
-            </div>
-            <div class="card-footer bg-transparent border-top">
-                <div class="contact-links d-flex font-size-20">
-                    <div class="flex-fill">
-                        <a href="javascript: void(0);"><i class="bx bx-user-circle"></i></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-xl-3 col-sm-6">
-        <div class="card text-center">
-            <div class="card-body">
-                <div class="avatar-sm mx-auto mb-4">
-                    <span class="avatar-title rounded-circle bg-warning-subtle text-warning font-size-16">
-                        D
-                    </span>
-                </div>
-                <h5 class="font-size-15 mb-1"><a href="javascript: void(0);" class="text-dark">David McHenry</a></h5>
-                <p class="text-muted">Smkn 1 kraksaan</p>
-                <div>
-                    <a href="javascript: void(0);" class="badge bg-warning font-size-11 m-1">Penanggung Jawab</a>
-                </div>
-            </div>
-            <div class="card-footer bg-transparent border-top">
-                <div class="contact-links d-flex font-size-20">
-                    <div class="flex-fill">
-                        <a href="javascript: void(0);"><i class="bx bx-user-circle"></i></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-xl-3 col-sm-6">
-        <div class="card text-center">
-            <div class="card-body">
-                <div class="avatar-sm mx-auto mb-4">
-                    <span class="avatar-title rounded-circle bg-warning-subtle text-warning font-size-16">
-                        D
-                    </span>
-                </div>
-                <h5 class="font-size-15 mb-1"><a href="javascript: void(0);" class="text-dark">David McHenry</a></h5>
-                <p class="text-muted">Smkn 1 kraksaan</p>
-                <div>
-                    <a href="javascript: void(0);" class="badge bg-warning font-size-11 m-1">Penanggung Jawab</a>
-                </div>
-            </div>
-            <div class="card-footer bg-transparent border-top">
-                <div class="contact-links d-flex font-size-20">
-                    <div class="flex-fill">
-                        <a href="javascript: void(0);"><i class="bx bx-user-circle"></i></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-xl-3 col-sm-6">
-        <div class="card text-center">
-            <div class="card-body">
-                <div class="avatar-sm mx-auto mb-4">
-                    <span class="avatar-title rounded-circle bg-warning-subtle text-warning font-size-16">
-                        D
-                    </span>
-                </div>
-                <h5 class="font-size-15 mb-1"><a href="javascript: void(0);" class="text-dark">David McHenry</a></h5>
-                <p class="text-muted">Smkn 1 kraksaan</p>
-                <div>
-                    <a href="javascript: void(0);" class="badge bg-warning font-size-11 m-1">Penanggung Jawab</a>
-                </div>
-            </div>
-            <div class="card-footer bg-transparent border-top">
-                <div class="contact-links d-flex font-size-20">
-                    <div class="flex-fill">
-                        <a href="javascript: void(0);"><i class="bx bx-user-circle"></i></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-xl-3 col-sm-6">
-        <div class="card text-center">
-            <div class="card-body">
-                <div class="avatar-sm mx-auto mb-4">
-                    <span class="avatar-title rounded-circle bg-warning-subtle text-warning font-size-16">
-                        D
-                    </span>
-                </div>
-                <h5 class="font-size-15 mb-1"><a href="javascript: void(0);" class="text-dark">David McHenry</a></h5>
-                <p class="text-muted">Smkn 1 kraksaan</p>
-                <div>
-                    <a href="javascript: void(0);" class="badge bg-warning font-size-11 m-1">Penanggung Jawab</a>
-                </div>
-            </div>
-            <div class="card-footer bg-transparent border-top">
-                <div class="contact-links d-flex font-size-20">
-                    <div class="flex-fill">
-                        <a href="javascript: void(0);"><i class="bx bx-user-circle"></i></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-xl-3 col-sm-6">
-        <div class="card text-center">
-            <div class="card-body">
-                <div class="avatar-sm mx-auto mb-4">
-                    <span class="avatar-title rounded-circle bg-warning-subtle text-warning font-size-16">
-                        D
-                    </span>
-                </div>
-                <h5 class="font-size-15 mb-1"><a href="javascript: void(0);" class="text-dark">David McHenry</a></h5>
-                <p class="text-muted">Smkn 1 kraksaan</p>
-                <div>
-                    <a href="javascript: void(0);" class="badge bg-warning font-size-11 m-1">Penanggung Jawab</a>
-                </div>
-            </div>
-            <div class="card-footer bg-transparent border-top">
-                <div class="contact-links d-flex font-size-20">
-                    <div class="flex-fill">
-                        <a href="javascript: void(0);"><i class="bx bx-user-circle"></i></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-xl-3 col-sm-6">
-        <div class="card text-center">
-            <div class="card-body">
-                <div class="avatar-sm mx-auto mb-4">
-                    <span class="avatar-title rounded-circle bg-warning-subtle text-warning font-size-16">
-                        D
-                    </span>
-                </div>
-                <h5 class="font-size-15 mb-1"><a href="javascript: void(0);" class="text-dark">David McHenry</a></h5>
-                <p class="text-muted">Smkn 1 kraksaan</p>
-                <div>
-                    <a href="javascript: void(0);" class="badge bg-warning font-size-11 m-1">Penanggung Jawab</a>
-                </div>
-            </div>
-            <div class="card-footer bg-transparent border-top">
-                <div class="contact-links d-flex font-size-20">
-                    <div class="flex-fill">
-                        <a href="javascript: void(0);"><i class="bx bx-user-circle"></i></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-xl-3 col-sm-6">
-        <div class="card text-center">
-            <div class="card-body">
-                <div class="avatar-sm mx-auto mb-4">
-                    <span class="avatar-title rounded-circle bg-warning-subtle text-warning font-size-16">
-                        D
-                    </span>
-                </div>
-                <h5 class="font-size-15 mb-1"><a href="javascript: void(0);" class="text-dark">David McHenry</a></h5>
-                <p class="text-muted">Smkn 1 kraksaan</p>
-                <div>
-                    <a href="javascript: void(0);" class="badge bg-warning font-size-11 m-1">Penanggung Jawab</a>
-                </div>
-            </div>
-            <div class="card-footer bg-transparent border-top">
-                <div class="contact-links d-flex font-size-20">
-                    <div class="flex-fill">
-                        <a href="javascript: void(0);"><i class="bx bx-user-circle"></i></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-xl-3 col-sm-6">
-        <div class="card text-center">
-            <div class="card-body">
-                <div class="avatar-sm mx-auto mb-4">
-                    <span class="avatar-title rounded-circle bg-warning-subtle text-warning font-size-16">
-                        D
-                    </span>
-                </div>
-                <h5 class="font-size-15 mb-1"><a href="javascript: void(0);" class="text-dark">David McHenry</a></h5>
-                <p class="text-muted">Smkn 1 kraksaan</p>
-                <div>
-                    <a href="javascript: void(0);" class="badge bg-warning font-size-11 m-1">Penanggung Jawab</a>
-                </div>
-            </div>
-            <div class="card-footer bg-transparent border-top">
-                <div class="contact-links d-flex font-size-20">
-                    <div class="flex-fill">
-                        <a href="javascript: void(0);"><i class="bx bx-user-circle"></i></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-xl-3 col-sm-6">
-        <div class="card text-center">
-            <div class="card-body">
-                <div class="avatar-sm mx-auto mb-4">
-                    <span class="avatar-title rounded-circle bg-warning-subtle text-warning font-size-16">
-                        D
-                    </span>
-                </div>
-                <h5 class="font-size-15 mb-1"><a href="javascript: void(0);" class="text-dark">David McHenry</a></h5>
-                <p class="text-muted">Smkn 1 kraksaan</p>
-                <div>
-                    <a href="javascript: void(0);" class="badge bg-warning font-size-11 m-1">Penanggung Jawab</a>
-                </div>
-            </div>
-            <div class="card-footer bg-transparent border-top">
-                <div class="contact-links d-flex font-size-20">
-                    <div class="flex-fill">
-                        <a href="javascript: void(0);"><i class="bx bx-user-circle"></i></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-xl-3 col-sm-6">
-        <div class="card text-center">
-            <div class="card-body">
-                <div class="avatar-sm mx-auto mb-4">
-                    <span class="avatar-title rounded-circle bg-warning-subtle text-warning font-size-16">
-                        D
-                    </span>
-                </div>
-                <h5 class="font-size-15 mb-1"><a href="javascript: void(0);" class="text-dark">David McHenry</a></h5>
-                <p class="text-muted">Smkn 1 kraksaan</p>
-                <div>
-                    <a href="javascript: void(0);" class="badge bg-warning font-size-11 m-1">Penanggung Jawab</a>
-                </div>
-            </div>
-            <div class="card-footer bg-transparent border-top">
-                <div class="contact-links d-flex font-size-20">
-                    <div class="flex-fill">
-                        <a href="javascript: void(0);"><i class="bx bx-user-circle"></i></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    @endforeach
 </div>
 <!-- end row -->
 
