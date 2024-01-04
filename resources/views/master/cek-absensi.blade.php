6373f0d628d79587c3dcf8d74b2e9a700062e97e
Kader2637 <kadergantengrek@gmail.com>
2024-01-04 09:58:10 +0700

    Update cek-absensi.blade.php

--- resources/views/master/cek-absensi.blade.php
+++ resources/views/master/cek-absensi.blade.php
@@ -66,7 +66,7 @@
                                 <td>{{ $loop->iteration }}</td>
                                 <td>{{ $student->name }}</td>
                                 <td>{{ $student->sekolah }}</td>
-                                <td>{{ \Carbon\Carbon::parse($student->created_at)->format('d F Y') }}</td>
+                                <td>{{ \Carbon\Carbon::parse( request()->date )->format('d F Y') }}</td>
                                 <td>
                                     @if (isset($student->attendances[0]))
                                         @if ($student->attendances[0]->status == 'masuk')
