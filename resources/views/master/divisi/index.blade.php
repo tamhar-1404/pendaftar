9f57bded03f330ca072690315934f20b08629789
rahmat <rahmatmahendra888@gmail.com>
2024-01-06 11:51:08 +0700

    divisi

---
+++ resources/views/master/divisi/index.blade.php
@@ -0,0 +1,210 @@
+@extends('layout.app')
+@section('content')
+<div class="row">
+    <div class="col-12">
+        <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+            <h4 class="mb-sm-0 font-size-18">Divisi</h4>
+        </div>
+    </div>
+</div>
+    <div class="card">
+        <div class="card-body">
+            <div class="d-flex justify-content-between mb-4">
+                <div class="">
+                    <h4 class="card-title">Divisi</h4>
+                </div>
+                <div class="">
+                    <button class="btn btn-info">Tambah Data</button>
+                </div>
+            </div>
+            <div class="d-flex justify-content-between">
+                <div class="">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xl-4 col-xxl-3 col-lg-6 col-md-6 col-sm-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex">
+                                <div class="flex-shrink-0 me-2">
+                                    <div class="avatar-xs">
+                                        <span class="avatar-title rounded-circle bg-success text-white font-size-16">
+                                            M
+                                        </span>
+                                    </div>
+                                </div>
+                                <div class="row overflow-hidden align-items-center">
+                                    <h5 class="text-truncate font-size-15 m-0"><a href="javascript: void(0);" class="text-dark">Mobile</a></h5>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="px-4 py-3 border-top d-flex ">
+                            <ul class="list-inline mb-0">
+                                <li class="list-inline-item me-0">
+                                    <i class= "bx bx-calendar me-0"></i> 15 jan, 2023
+                                </li>
+                            </ul>
+                            <div class="d-flex ms-auto gap-2">
+                                <span>
+                                    <button data-bs-toggle="modal"
+                                        class=" btn btn-sm btn-info"
+                                        style="">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 48 48"><g fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="4"><path stroke-linecap="round" d="M7 42h36"/><path fill="currentColor" d="M11 26.72V34h7.317L39 13.308L31.695 6z"/></g></svg>
+                                    </button>
+                                </span>
+                                <span>
+                                    <button data-bs-toggle="modal"
+                                        class=" btn btn-sm btn-danger"
+                                        style="">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 48 48"><defs><mask id="ipSDelete0"><g fill="none" stroke-linejoin="round" stroke-width="4"><path fill="#fff" stroke="#fff" d="M9 10v34h30V10z"/><path stroke="#000" stroke-linecap="round" d="M20 20v13m8-13v13"/><path stroke="#fff" stroke-linecap="round" d="M4 10h40"/><path fill="#fff" stroke="#fff" d="m16 10l3.289-6h9.488L32 10z"/></g></mask></defs><path fill="currentColor" d="M0 0h48v48H0z" mask="url(#ipSDelete0)"/></svg>
+                                    </button>
+                                </span>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-xxl-3 col-lg-6 col-md-6 col-sm-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex">
+                                <div class="flex-shrink-0 me-2">
+                                    <div class="avatar-xs">
+                                        <span class="avatar-title rounded-circle bg-success text-white font-size-16">
+                                            M
+                                        </span>
+                                    </div>
+                                </div>
+                                <div class="row overflow-hidden align-items-center">
+                                    <h5 class="text-truncate font-size-15 m-0"><a href="javascript: void(0);" class="text-dark">Mobile</a></h5>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="px-4 py-3 border-top d-flex ">
+                            <ul class="list-inline mb-0">
+                                <li class="list-inline-item me-0">
+                                    <i class= "bx bx-calendar me-0"></i> 15 jan, 2023
+                                </li>
+                            </ul>
+                            <div class="d-flex ms-auto gap-2">
+                                <span>
+                                    <button data-bs-toggle="modal"
+                                        class=" btn btn-sm btn-info"
+                                        style="">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 48 48"><g fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="4"><path stroke-linecap="round" d="M7 42h36"/><path fill="currentColor" d="M11 26.72V34h7.317L39 13.308L31.695 6z"/></g></svg>
+                                    </button>
+                                </span>
+                                <span>
+                                    <button data-bs-toggle="modal"
+                                        class=" btn btn-sm btn-danger"
+                                        style="">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 48 48"><defs><mask id="ipSDelete0"><g fill="none" stroke-linejoin="round" stroke-width="4"><path fill="#fff" stroke="#fff" d="M9 10v34h30V10z"/><path stroke="#000" stroke-linecap="round" d="M20 20v13m8-13v13"/><path stroke="#fff" stroke-linecap="round" d="M4 10h40"/><path fill="#fff" stroke="#fff" d="m16 10l3.289-6h9.488L32 10z"/></g></mask></defs><path fill="currentColor" d="M0 0h48v48H0z" mask="url(#ipSDelete0)"/></svg>
+                                    </button>
+                                </span>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-xxl-3 col-lg-6 col-md-6 col-sm-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex">
+                                <div class="flex-shrink-0 me-2">
+                                    <div class="avatar-xs">
+                                        <span class="avatar-title rounded-circle bg-success text-white font-size-16">
+                                            W
+                                        </span>
+                                    </div>
+                                </div>
+                                <div class="row overflow-hidden align-items-center">
+                                    <h5 class="text-truncate font-size-15 m-0"><a href="javascript: void(0);" class="text-dark">Web Devlopment</a></h5>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="px-4 py-3 border-top d-flex ">
+                            <ul class="list-inline mb-0">
+                                <li class="list-inline-item me-0">
+                                    <i class= "bx bx-calendar me-0"></i> 15 jan, 2023
+                                </li>
+                            </ul>
+                            <div class="d-flex ms-auto gap-2">
+                                <span>
+                                    <button data-bs-toggle="modal"
+                                        class=" btn btn-sm btn-info"
+                                        style="">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 48 48"><g fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="4"><path stroke-linecap="round" d="M7 42h36"/><path fill="currentColor" d="M11 26.72V34h7.317L39 13.308L31.695 6z"/></g></svg>
+                                    </button>
+                                </span>
+                                <span>
+                                    <button data-bs-toggle="modal"
+                                        class=" btn btn-sm btn-danger"
+                                        style="">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 48 48"><defs><mask id="ipSDelete0"><g fill="none" stroke-linejoin="round" stroke-width="4"><path fill="#fff" stroke="#fff" d="M9 10v34h30V10z"/><path stroke="#000" stroke-linecap="round" d="M20 20v13m8-13v13"/><path stroke="#fff" stroke-linecap="round" d="M4 10h40"/><path fill="#fff" stroke="#fff" d="m16 10l3.289-6h9.488L32 10z"/></g></mask></defs><path fill="currentColor" d="M0 0h48v48H0z" mask="url(#ipSDelete0)"/></svg>
+                                    </button>
+                                </span>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-xxl-3 col-lg-6 col-md-6 col-sm-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex">
+                                <div class="flex-shrink-0 me-2">
+                                    <div class="avatar-xs">
+                                        <span class="avatar-title rounded-circle bg-success text-white font-size-16">
+                                            M
+                                        </span>
+                                    </div>
+                                </div>
+                                <div class="row overflow-hidden align-items-center">
+                                    <h5 class="text-truncate font-size-15 m-0"><a href="javascript: void(0);" class="text-dark">Mobile</a></h5>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="px-4 py-3 border-top d-flex ">
+                            <ul class="list-inline mb-0">
+                                <li class="list-inline-item me-0">
+                                    <i class= "bx bx-calendar me-0"></i> 15 jan, 2023
+                                </li>
+                            </ul>
+                            <div class="d-flex ms-auto gap-2">
+                                <span>
+                                    <button data-bs-toggle="modal"
+                                        class=" btn btn-sm btn-info"
+                                        style="">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 48 48"><g fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="4"><path stroke-linecap="round" d="M7 42h36"/><path fill="currentColor" d="M11 26.72V34h7.317L39 13.308L31.695 6z"/></g></svg>
+                                    </button>
+                                </span>
+                                <span>
+                                    <button data-bs-toggle="modal"
+                                        class=" btn btn-sm btn-danger"
+                                        style="">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 48 48"><defs><mask id="ipSDelete0"><g fill="none" stroke-linejoin="round" stroke-width="4"><path fill="#fff" stroke="#fff" d="M9 10v34h30V10z"/><path stroke="#000" stroke-linecap="round" d="M20 20v13m8-13v13"/><path stroke="#fff" stroke-linecap="round" d="M4 10h40"/><path fill="#fff" stroke="#fff" d="m16 10l3.289-6h9.488L32 10z"/></g></mask></defs><path fill="currentColor" d="M0 0h48v48H0z" mask="url(#ipSDelete0)"/></svg>
+                                    </button>
+                                </span>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+@endsection
+@section('script')
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.css" />
+    <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.js"></script>
+
+    <script>
+        new DataTable('#tech-companies-1');
+    </script>
+@endsection
