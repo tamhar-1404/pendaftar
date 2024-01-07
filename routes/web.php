652b87e700331db21315ecb07b71ee65a59a1aba
rahmat <rahmatmahendra888@gmail.com>
2024-01-07 10:47:43 +0700

    persentasi

--- routes/web.php
+++ routes/web.php
@@ -87,6 +87,7 @@ Route::get('/selesai', [LoginController::class, 'selesai'])->name('selesai');
 // Rute untuk mengirim email reset password
 Route::get('/', function () {return view('landing-page.index');});
 Route::get('/menu-divisi', function () {return view('master.divisi.index');});
+Route::get('/menu-persentasi', function () {return view('master.persentasi.index');});
 
 Route::middleware(['auth'])->group(function () {
     Route::middleware(['role:Admin'])->group(function () {
